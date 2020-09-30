5616f6135c6fdae38188bda6edaf6997f689d370
root <getman.sergei@gmail.com>
2020-09-30 17:51:00 +0000

    update molokai copying

---
+++ vim-go-config.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.vim 
+
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp.vim
+
+mkdir -p  ~/.vim/colors 
+cp colors/molokai.vim ~/.vim/colors/molokai.vim 
