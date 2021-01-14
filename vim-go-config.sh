9ae08785249925532a6c6cf389288e3df8392beb
Sergii Getman <getman.sergei@gmail.com>
2021-01-14 14:16:34 +0000

    Update vim-go-config.sh

--- vim-go-config.sh
+++ vim-go-config.sh
@@ -1,14 +1,11 @@
 mkdir -p ~/.vim 
 
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp.vim
-
 git clone https://github.com/mileszs/ack.vim.git ~/.vim/pack/plugins/start/ack.vim
-
 git clone https://github.com/Shougo/neosnippet.git ~/.vim/pack/plugins/start/neosnippet.vim
-
 git clone https://github.com/Shougo/neosnippet-snippets.git ~/.vim/pack/plugins/start/neosnippet-snippets.vim
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/plugins/start/vim-airline
 
 mkdir -p  ~/.vim/colors 
 cp colors/molokai.vim ~/.vim/colors/molokai.vim 
