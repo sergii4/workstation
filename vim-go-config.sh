b3c45c72c89745147bc357f47b45dcbc9c3e88ff
Sergii Getman <getman.sergei@gmail.com>
2021-02-13 17:10:23 +0000

    Update vim-go-config.sh

--- vim-go-config.sh
+++ vim-go-config.sh
@@ -8,12 +8,6 @@ git clone https://github.com/Shougo/neosnippet.git ~/.vim/pack/plugins/start/neo
 git clone https://github.com/Shougo/neosnippet-snippets.git ~/.vim/pack/plugins/start/neosnippet-snippets.vim
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/plugins/start/vim-airline
 
-# javasript autocomplete
-git clone https://github.com/ternjs/tern_for_vim.git ~/.vim/pack/plugins/start/tern_for_vim
-
-# python autocomplete
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/pack/plugins/opt/jedi-vim
-
 # color scheme
 mkdir -p  ~/.vim/colors 
 cp colors/molokai.vim ~/.vim/colors/molokai.vim 
